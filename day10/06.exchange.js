46e93928f41821ddf1e4a3d6e62bd37bbd0977bd
KANGMIYEON <myeonk56@gmail.com>
2022-12-29 14:42:21 +0900

    first commit

---
+++ day10/06.exchange.js
@@ -0,0 +1,60 @@
+const top1 = {
+
+    exchanges:[
+        {
+            name: '미국USD',
+            point: 1418.80,
+            updown: -8.20
+        },
+        {
+            name: '일본JPY (100엔)',
+            point: 959.26,
+            updown: -6.27
+        },
+        {
+            name: '유럽연합EUR',
+            point: 1387.87,
+            updown: -9.45
+        },
+        {
+            name: '중국CNY',
+            point: 195.30,
+            updown: 0.54
+        }
+    ],
+
+    gold:[
+        {
+            name: '국제금',
+            price: 1630.90,
+            ratio: -19.10
+        },
+        {
+            name: '국내금',
+            price: 75168.82,
+            ratio: 856.52
+        }
+    ],
+
+
+    "거래 상위":[
+        {
+            name: 'KODEX 200선물인버스2X',
+            price: 3305,
+            down: 15,
+            ratio: 0.61
+        },
+        {
+            name: '신한 인버스 2X 천연가스',
+            price: 175,
+            down: 25,
+            ratio: 16.67
+        },
+        {
+          
+        }, { }, { }, { }
+    ],
+    "상승":[],
+    "하락":[],
+    "시가총액 상위":[]
+};
